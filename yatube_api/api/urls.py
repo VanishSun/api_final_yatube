dd7e4f0c13dd95fa5302502fa9b15b02be6c9bea
Иван Верозуб <vanish.sun@gmail.com>
2022-12-07 22:50:26 +0500

    mistype fixed, commented DEBUG settings

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework import routers
 
-from posts.views import (
+from api.views import (
     CommentViewSet,
     FollowViewSet,
     GroupViewSet,
@@ -15,7 +15,7 @@ router_v1.register('posts', PostViewSet, basename='posts')
 router_v1.register('groups', GroupViewSet, basename='groups')
 router_v1.register('follow', FollowViewSet, basename='follow')
 router_v1.register(
-    r'posts\/(?<post_id>\d+)\/comments',
+    r'posts\/(?P<post_id>\d+)\/comments',
     CommentViewSet,
     basename='comments'
 )
@@ -25,8 +25,8 @@ urlpatterns = [
     path('v1/', include('djoser.urls.jwt')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         settings.MEDIA_URL,
+#         document_root=settings.MEDIA_ROOT
+#     )
