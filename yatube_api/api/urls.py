14aa22411a1bcea361db705d7beeb41f4ea95da4
Иван Верозуб <vanish.sun@gmail.com>
2022-12-07 17:34:27 +0500

    added jwt & router_v1 urls; added if set.DEBUG

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,32 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
+from rest_framework import routers
+
+from posts.views import (
+    CommentViewSet,
+    FollowViewSet,
+    GroupViewSet,
+    PostViewSet,
+)
+
+router_v1 = routers.DefaultRouter()
+router_v1.register('posts', PostViewSet, basename='posts')
+router_v1.register('groups', GroupViewSet, basename='groups')
+router_v1.register('follow', FollowViewSet, basename='follow')
+router_v1.register(
+    r'posts\/(?<post_id>\d+)\/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    path('v1/', include('djoser.urls.jwt')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
