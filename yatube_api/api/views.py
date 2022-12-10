c134e10e112993cffe0c1ae73a8621aab746994f
Иван Верозуб <vanish.sun@gmail.com>
2022-12-10 17:25:23 +0500

    changed permissions for posts & comments to default in settings

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -16,7 +16,6 @@ from .serializers import (
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
     pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
@@ -30,7 +29,6 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
