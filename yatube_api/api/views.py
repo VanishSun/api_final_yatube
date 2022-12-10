991924dd9beacc736174e22c2a0a5f478ae04cef
Иван Верозуб <vanish.sun@gmail.com>
2022-12-10 17:26:09 +0500

    removed not used import

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,7 +4,6 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import mixins, GenericViewSet
 
 from posts.models import Group, Post
-from .permissions import IsAuthorOrReadOnly
 from .serializers import (
     CommentSerializer,
     FollowSerializer,
