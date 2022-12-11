5c3584f985c0ef0b36729cfe3a1d7abe1305a768
Иван Верозуб <vanish.sun@gmail.com>
2022-12-11 20:01:54 +0500

    changed AnonimousUser() to .is_authenticated

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,12 +1,11 @@
 from rest_framework import permissions
-from django.contrib.auth.models import AnonymousUser
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user != AnonymousUser()
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
